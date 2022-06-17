91a4bd32f07be2ad233dc870710be6f5b0dbaa56
Edu-Lu <moralesluis17106494@gmail.com>
2022-06-17 12:08:44 -0500

    Problema resuelto

--- app/src/main/java/com/luismorales17106494/radiogroupyradiobutton/MainActivity.kt
+++ app/src/main/java/com/luismorales17106494/radiogroupyradiobutton/MainActivity.kt
@@ -1,14 +1,23 @@
 package com.luismorales17106494.radiogroupyradiobutton
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.view.isNotEmpty
+import com.luismorales17106494.radiogroupyradiobutton.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         /*
         4 - Controles RadioGroup y RadioButton
+
         Problema:
         Realizar la carga de dos números en controles de tipo EditText.
         Mostrar mensajes que soliciten la carga de los valores dentro de los
@@ -22,6 +31,57 @@ class MainActivity : AppCompatActivity() {
         RadioGroup (este control se encuentra en la paleta de componentes en la
         pestaña Buttons de la "Palette"):
          */
+
+        binding.btOperar.setOnClickListener() {
+            if (binding.etNumero1.text.isNotBlank() && binding.etNumero2.text.isNotBlank()) {
+
+
+                Toast.makeText(
+                    this,
+                    binding.rgbGrop.isClickable.toString(),
+                    Toast.LENGTH_LONG
+                ).show()
+                when {
+                    binding.rbRestar.isChecked -> {
+                        binding.tvResultado.text = "" +
+                                "${
+                                    binding.etNumero1.text.toString()
+                                        .toDouble() - binding.etNumero2.text.toString().toDouble()
+                                }"
+                    }
+                    binding.rbSumar.isChecked -> {
+                        binding.tvResultado.text = "" +
+                                "${
+                                    binding.etNumero1.text.toString()
+                                        .toDouble() + binding.etNumero2.text.toString().toDouble()
+                                }"
+                    }
+                    binding.rbMulti.isChecked -> {
+                        binding.tvResultado.text = "" +
+                                "${
+                                    binding.etNumero1.text.toString()
+                                        .toDouble() * binding.etNumero2.text.toString().toDouble()
+                                }"
+                    }
+                    binding.rbDividir.isChecked -> {
+                        binding.tvResultado.text = "" +
+                                "${
+                                    binding.etNumero1.text.toString()
+                                        .toDouble() / binding.etNumero2.text.toString().toDouble()
+                                }"
+                    }
+                }
+
+
+            } else {
+                Toast.makeText(
+                    this, "Rellene los dos campos", Toast.LENGTH_SHORT
+                ).show()
+            }
+
+
+        }
+
 
     }
 }
